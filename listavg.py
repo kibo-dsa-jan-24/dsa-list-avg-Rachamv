60540264a2073bafa4b0201828e2b48e131f34d8
Rachamv <rkutalian@gmail.com>
2024-01-11 15:51:15 +0100

    hmmmmm

--- listavg.py
+++ listavg.py
@@ -1,9 +1,13 @@
 class ListAverage:
     def __init__(self, lst):
         self.lst = lst.copy()
+        self.total_sum = 0
+        for num in lst:
+            self.total_sum += num
 
     def add(self, num):
         self.lst.append(num)
+        self.total_sum += num
 
     def compute_avg(self):
         total = 0
@@ -12,5 +16,5 @@ class ListAverage:
         return total / len(self.lst)
 
     def compute_avg_faster(self):
-        # implement this method
-        pass
+        scaled_average = round(self.total_sum * 10 / len(self.lst))
+        return scaled_average / 10
--- test_listavg.py
+++ test_listavg.py
@@ -2,8 +2,43 @@ import unittest
 from listavg import ListAverage
 
 class TestListAverage(unittest.TestCase):
-    def test(self):
+
+    # Test empty list
+    def test_empty_list(self):
+        lavg = ListAverage([])
+        assert lavg.compute_avg() == 0.0
+        assert lavg.compute_avg_faster() == 0.0
+
+    # Test single element list
+    def test_single_element(self):
+        lavg = ListAverage([5])
+        assert lavg.compute_avg() == 5.0
+        assert lavg.compute_avg_faster() == 5.0
+
+    # Test negative numbers
+    def test_negative_numbers(self):
+        lavg = ListAverage([-2, -4, 6])
+        assert lavg.compute_avg() == 0.0
+        assert lavg.compute_avg_faster() == 0.0
+
+    # Test decimal numbers
+    def test_decimal_numbers(self):
+        lavg = ListAverage([1.5, 2.3, 3.7])
+        assert lavg.compute_avg() == 2.5
+        assert lavg.compute_avg_faster() == 2.5
+
+    # Test large numbers
+    def test_large_numbers(self):
+        lavg = ListAverage([100000, 200000, 300000])
+        assert lavg.compute_avg() == 200000.0
+        assert lavg.compute_avg_faster() == 200000.0
+
+    # Test adding numbers
+    def test_adding_numbers(self):
         lavg = ListAverage([1, 2, 3])
         assert lavg.compute_avg() == 2.0
+        assert lavg.compute_avg_faster() == 2.0
+        lavg.add(4)
+        assert lavg.compute_avg() == 2.5
+        assert lavg.compute_avg_faster() == 2.5
 
-    # add more unit tests below
